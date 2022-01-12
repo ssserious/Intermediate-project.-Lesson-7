e39bde715bb81ad20fe652013e54e270bf7485b1
ssserious <55954522+ssserious@users.noreply.github.com>
2022-01-12 03:53:29 +0300

    Add files via upload

---
+++ WeatherViewer.java
@@ -0,0 +1,39 @@
+/**
+ * Класс WeatherController
+ * @author : Sergey Sergeev
+ * @version 22.12.2021
+ */
+
+package view;
+
+import java.io.IOException;
+import java.util.*;
+import model.*;
+import controller.WeatherController;
+
+public class WeatherViewer {
+
+    public static void consoleView(WeatherResponse data){
+        System.out.println(data);
+    }
+
+    public static void processCity() throws IOException {
+        // бесконечный цикл опроса ввода из консоли.
+        while (true) {
+            System.out.println("--= Введите город (для выхода из программы наберите 'exit') =--");
+            Scanner sc = new Scanner(System.in);
+            String city = sc.nextLine();
+            if (city.equals("exit")) {
+                break;
+            } else {
+                WeatherResponse weather = WeatherController.getWeatherFromCityV2(city);
+                if (weather.isEmpty()) {
+                    System.out.println("Такого города нет в OpenWeatherMap или данные по нему не могут быть предоставлены");
+                } else {
+                    consoleView(WeatherController.getWeatherFromCityV2(city));
+                }
+            }
+        }
+    }
+
+}
