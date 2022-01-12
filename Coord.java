2960cf854c96f098bbba39527e482aff916aadcc
ssserious <55954522+ssserious@users.noreply.github.com>
2022-01-12 03:53:16 +0300

    Add files via upload

---
+++ Coord.java
@@ -0,0 +1,33 @@
+/**
+ * Класс Coord
+ *@author : Sergey Sergeev
+ *@version 22.12.2021
+ */
+
+package model;
+
+import java.util.*;
+
+public class Coord {           // координаты места
+    Double lat;
+    Double lon;
+
+    public Coord() {
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+}
---
+++ Sys.java
@@ -0,0 +1,23 @@
+/**
+ * Класс Sys
+ *@author : Sergey Sergeev
+ *@version 22.12.2021
+ */
+package model;
+
+import java.util.*;
+
+public class Sys {
+    String pod;
+
+    public Sys() {
+    }
+
+    public String getPod() {
+        return pod;
+    }
+
+    public void setPod(String pod) {
+        this.pod = pod;
+    }
+}
